e3dc243179571fae89bedc0b8b7654560d0cb294
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-06-15 22:33:27 +0200

    updated dtslint, test now returns no errors

--- dist/game/index.d.ts
+++ dist/game/index.d.ts
@@ -26,5 +26,6 @@ declare module "game" {
     season: string;
   };
 
-  export function createConstructionSite(x: number, y: number, structurePrototype: STRUCTURE_PROTOTYPES):{ object?: Structure; error?: ERR_BUSY | ERR_INVALID_ARGS | ERR_NOT_ENOUGH_ENERGY };
+  export function createConstructionSite(x: number, y: number, structurePrototype: string /*STRUCTURE_PROTOTYPES*/):
+  { object?: Structure; error?: ERR_BUSY | ERR_INVALID_ARGS | ERR_NOT_ENOUGH_ENERGY };
 }
