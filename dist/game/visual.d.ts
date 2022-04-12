0f10bf0633dcce71a6f98cc24ad25f3875045a4f
thmsn <9084377+thmsndk@users.noreply.github.com>
2022-04-12 17:01:46 +0200

    Fixed a few linting errors due to too logn descriptions

--- dist/game/visual.d.ts
+++ dist/game/visual.d.ts
@@ -2,7 +2,9 @@ declare module "game/visual" {
   import { RoomPosition, _Constructor } from "game/prototypes";
 
   /**
-   * Visuals provide a way to show various visual debug info in the game. All drawings will be shown for the current tick only. All draw coordinates are measured in game coordinates and centered to tile centers, i.e. (10,10) will point to the center of the creep at x:10; y:10 position. Fractional coordinates are allowed.
+   * Visuals provide a way to show various visual debug info in the game.
+   * All drawings will be shown for the current tick only.
+   * All draw coordinates are measured in game coordinates and centered to tile centers, i.e. (10,10) will point to the center of the creep at x:10; y:10 position. Fractional coordinates are allowed.
    */
   export interface Visual {
     /**
@@ -42,9 +44,9 @@ declare module "game/visual" {
 
     /**
      * Draw a rectangle.
-     * @param topLeftPos 	The position object of the top-left corner. May be GameObject or any object containing x and y properties.
+     * @param topLeftPos The position object of the top-left corner. May be GameObject or any object containing x and y properties.
      * @param width The width of the rectangle.
-     * @param height 	The height of the rectangle.
+     * @param height The height of the rectangle.
      * @param style An object with the following properties:
      * - radius (number) Circle radius, default is 0.15.
      * - fill (string) Fill color in the following format: #ffffff (hex triplet). Default is #ffffff.
@@ -76,10 +78,12 @@ declare module "game/visual" {
      * @param pos The position object of the label baseline. May be GameObject or any object containing x and y properties.
      * @param style An object with the following properties:
      * - color (string) Font color in the following format: #ffffff (hex triplet). Default is #ffffff.
-     * - font (number|string) Either a number or a string in one of the following forms: "0.7" (relative size in game coordinates), "20px" (absolute size in pixels), "0.7 serif", or "bold italic 1.5 Times New Roman"
+     * - font (number|string) Either a number or a string in one of the following forms: "0.7" (relative size in game coordinates)
+     * , "20px" (absolute size in pixels), "0.7 serif", or "bold italic 1.5 Times New Roman"
      * - stroke (string) Stroke color in the following format: #ffffff (hex triplet). default is undefined (no stroke).
      * - strokeWidth (number) Stroke line width, default is 0.15.
-     * - backgroundColor (string) Background color in the following format: #ffffff (hex triplet). Default is undefined (no background). When background is enabled, text vertical align is set to middle (default is baseline).
+     * - backgroundColor (string) Background color in the following format: #ffffff (hex triplet).
+     * Default is undefined (no background). When background is enabled, text vertical align is set to middle (default is baseline).
      * - backgroundPadding (number) Background rectangle padding, default is 0.3.
      * - aling (string) Text align, either center, left, or right. Default is center.
      * - opacity (number) Opacity value, default is 1.
@@ -91,19 +95,17 @@ declare module "game/visual" {
     clear(): Visual;
   }
 
-  interface VisualConstructor
-  extends _Constructor<Visual> {
+  interface VisualConstructor extends _Constructor<Visual> {
     /**
      * Creates a new empty instance of Visual.
-     * @param layer 	The layer of visuals in this object. Visuals of higher layer overlaps visuals of lower layer. Default is 0.
+     * @param layer The layer of visuals in this object. Visuals of higher layer overlaps visuals of lower layer. Default is 0.
      * @param persistent Whether visuals in this object are persistent. Non-persistent visuals are visible during the current tick only.
      */
-    new (layer?:number, persistent?:boolean): Visual;
+    new (layer?: number, persistent?: boolean): Visual;
     (): Visual;
   }
 
-
-  export const Visual: VisualConstructor
+  export const Visual: VisualConstructor;
 }
 
 interface LineStyle {
