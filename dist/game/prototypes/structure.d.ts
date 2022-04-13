cb0440bef0d7af04e151af579a79b278fb89563c
thmsn <9084377+thmsndk@users.noreply.github.com>
2022-04-13 13:20:27 +0200

    structureType does not exist anymore #15

--- dist/game/prototypes/structure.d.ts
+++ dist/game/prototypes/structure.d.ts
@@ -24,28 +24,6 @@ declare module "game/prototypes" {
      * The total amount of hit points of the structure.
      */
     hitsMax: number;
-    // /**
-    //  * A unique object identifier. You can use Game.getObjectById method to retrieve an object instance by its id.
-    //  */
-    // id: Id<this>;
-
-    /**
-     * One of the STRUCTURE_* constants.
-     */
-    structureType: T;
-    // /**
-    //  * Destroy this structure immediately.
-    //  */
-    // destroy(): ScreepsReturnCode;
-    // /**
-    //  * Check whether this structure can be used. If the room controller level is not enough, then this method will return false, and the structure will be highlighted with red in the game.
-    //  */
-    // isActive(): boolean;
-    // /**
-    //  * Toggle auto notification when the structure is under attack. The notification will be sent to your account email. Turned on by default.
-    //  * @param enabled Whether to enable notification or disable.
-    //  */
-    // notifyWhenAttacked(enabled: boolean): ScreepsReturnCode;
 
     toJSON(): StructureJSON;
   }
