de88c9df32afd4fc80c977e49e6463ff5987b039
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-12-14 22:32:18 +0900

    add spawning to Creep

--- dist/game/prototypes/creep.d.ts
+++ dist/game/prototypes/creep.d.ts
@@ -49,6 +49,10 @@ declare module "game/prototypes" {
      */
     store: Store<ResourceConstant>;
     /**
+     * Whether it is spawning now.
+     */
+    spawning: boolean;
+    /**
      * Move the creep one square in the specified direction. direction must be one of the following constants:
      */
     move(direction: DirectionConstant): CreepMoveReturnCode;
