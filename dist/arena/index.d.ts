d8065862d579d3893f9be478ca4ea3fd6265762c
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-12-26 17:40:00 +0900

    add arena/season_alpha imports

--- dist/arena/index.d.ts
+++ dist/arena/index.d.ts
@@ -5,3 +5,27 @@ declare module "arena" {
   export * from "arena/prototypes";
   export * from "arena/constants";
 }
+
+declare module "arena/season_alpha/capture_the_flag/basic" {
+  export {Flag} from "arena/prototypes";
+}
+
+declare module "arena/season_alpha/capture_the_flag/advanced" {
+  export {Flag} from "arena/prototypes";
+}
+
+declare module "arena/season_alpha/collect_and_control/basic" {
+  export {AreaEffect, ScoreCollector} from "arena/prototypes";
+}
+
+declare module "arena/season_alpha/collect_and_control/basic/constants" {
+  export {EFFECT_FREEZE, RESOURCE_SCORE} from "arena/constants";
+}
+
+declare module "arena/season_alpha/collect_and_control/advanced" {
+  export {AreaEffect, ScoreCollector} from "arena/prototypes";
+}
+
+declare module "arena/season_alpha/collect_and_control/advanced/constants" {
+  export {EFFECT_DAMAGE, EFFECT_FREEZE, RESOURCE_SCORE_X, RESOURCE_SCORE_Y, RESOURCE_SCORE_Z} from "arena/constants";
+}
