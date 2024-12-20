df876a4909612e2223149060d2fd816fa6211a64
admon84 <admonwest84@gmail.com>
2024-12-19 21:08:04 -0700

    Fix arena index

--- dist/arena/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="season_beta/capture_the_flag/basic/prototypes/index.d.ts" />
-/// <reference path="season_beta/collect_and_control/basic/prototypes/index.d.ts" />
-/// <reference path="season_beta/collect_and_control/basic/constants/index.d.ts" />
-/// <reference path="season_beta/collect_and_control/advanced/constants/index.d.ts" />
-
-declare module "arena" {
-  export {};
-}
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,4 +5,7 @@
 // Minimum TypeScript Version: 3.8
 
 /// <reference path="game/index.d.ts" />
-/// <reference path="arena/index.d.ts" />
+/// <reference path="arena/season_beta/capture_the_flag/basic/prototypes/index.d.ts" />
+/// <reference path="arena/season_beta/collect_and_control/basic/prototypes/index.d.ts" />
+/// <reference path="arena/season_beta/collect_and_control/basic/constants/index.d.ts" />
+/// <reference path="arena/season_beta/collect_and_control/advanced/constants/index.d.ts" />
